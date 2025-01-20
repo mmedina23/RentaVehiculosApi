91153e2e40fabe159f66618a78b34d3e19b2aa86
mmedina23 <mariana.med23@gmail.com>
2025-01-20 19:44:23 +0100

    initial commit

---
+++ src/main/java/com/pmd/rentavehiculos/RentaVehiculosApiApplication.java
@@ -0,0 +1,13 @@
+package com.pmd.rentavehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaVehiculosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaVehiculosApiApplication.class, args);
+	}
+
+}
